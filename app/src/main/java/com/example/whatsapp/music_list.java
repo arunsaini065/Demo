c791c36f486bab390e8407ac78e18a311e62359e
rareprob <arunsaini065@gmail.com>
2019-06-28 10:44:25 +0530

    Demo

---
+++ app/src/main/java/com/example/whatsapp/music_list.java
@@ -0,0 +1,30 @@
+package com.example.whatsapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class music_list extends Fragment {
+
+   /* @Nullable
+    @Override*/
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_music_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
